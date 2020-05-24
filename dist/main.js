93059ca74e08af7295e812e6900576abd3653740
Clément Corbin <corbin.clement@gmail.com>
2020-05-24 15:03:28 +0000

    simplifying function generation & homogeneous naming

--- dist/main.js
+++ dist/main.js
@@ -1,62 +1,66 @@
-let makeRequestURL = (action,parameters=false) => {
-  let request = "./api?action="+action;
-  request += (parameters) ? "&options="+parameters:"";
+let makeRequestURL = (request,parameters=false) => {
+  request += (parameters) ? "?options="+parameters:"";
   return request
 };
 let makeRequestFunc = (request) => {
   let reqFunc;
   if (request.json) {
     reqFunc = async (param=(request.defaultValue || false)) => {
-      let req = await fetch(makeRequestURL(request.action,param));
+      let req = await fetch(makeRequestURL(request.name,param));
       req = await req.json();
       return req;
     }
   } else {
     if (request.await) {
       reqFunc = async (param) => {
-        await fetch(makeRequestURL(request.action,param));
+        await fetch(makeRequestURL(request.name,param));
       };
     } else if (request.urlonly) {
-      reqFunc = (param) => makeRequestURL(request.action,param);
-    }else {
-      reqFunc = (param) => { fetch(makeRequestURL(request.action,param)); };
+      reqFunc = (param) => makeRequestURL(request.name,param);
+    } else {
+      reqFunc = (param) => { fetch(makeRequestURL(request.name,param)); };
     }
   }
   return reqFunc;
 }
 class Jukebox {
   constructor() {
-    this.isStreaming = false;
+    //this.isStreaming = false;
     this.requests = [
-      {name:"search",json:true,defaultValue:""},
-      {name:"getTree",json:true,defaultValue:"./"},
-      {name:"getRadios",json:true},
-      {name:"playLive"},      //play live stream, eg. webradios
-      {name:"ytp"},           //youtube player
-      {name:"yts",json:true}, //youtube search
-      {name:"currentSong",json:true,action:"getCurrentSong"},
-      {name:"play",action:"playFile",defaultValue:"./"},
-      {name:"streamPlay",defaultValue:"./",urlonly:true},
-      {name:"playRandom",action:"playRandom",defaultValue:"./"},
-      {name:"stop"},
-      {name:"halt"},
-      {name:"allRandom",action:"playAllRandom"},
-      {name:"regenerate",action:"makeTree",await:true},
+      {name:"/files/search",json:true,defaultValue:""},
+      {name:"/files/list",json:true,defaultValue:"./"},
+      {name:"/radio/list",json:true},
+      {name:"/radio/play"},
+      {name:"/youtube/play"},
+      {name:"/youtube/search",json:true},
+      {name:"/player/log",json:true},
+      {name:"/player/play",defaultValue:"./"},
+      //{name:"streamPlay",defaultValue:"./",urlonly:true},
+      {name:"/player/shuffle",defaultValue:"./"},
+      {name:"/player/stop"},
+      {name:"/player/halt"},
+      {name:"/player/random"},
+      {name:"/files/regenerate",action:"makeTree",await:true},
     ];
     this.requests.map(e => {
-      e.action = (typeof e.action === "undefined")?e.name:e.action;
-      this[e.name] = makeRequestFunc(e);
+      let functionName = e.name.split("/").map((e,i) => {
+        if (i > 1) {
+          e = e[0].toUpperCase()+e.slice(1);
+        }
+        return e;
+      }).join("");
+      this[functionName] = makeRequestFunc(e);
     });
   }
   async upload(files) {
-    await fetch("./api", {method: "POST", body: files});
+    await fetch("/files/upload", {method: "POST", body: files});
   }
-  set stream(bool) {
+  /*set stream(bool) {
     this.isStreaming = (bool === true);
   }
   get stream() {
     return this.isStreaming;
-  }
+  }*/
 }
 let Jukeberry = new Jukebox();
 export { Jukeberry };
