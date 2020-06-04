6f5407ddd4aef280b4f876882ed558d5abf40fe8
Clément Corbin <corbin.clement@gmail.com>
2020-06-04 22:21:58 +0200

    basic routing

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MusicViewComponent } from './music-view/music-view.component';
+import { VideoViewComponent } from './video-view/video-view.component';
+import { RadioViewComponent } from './radio-view/radio-view.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'music', component: MusicViewComponent },
+  { path: 'video', component: VideoViewComponent },
+  { path: 'radio', component: RadioViewComponent },
+  { path: '', component: MusicViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicViewComponent,
+    VideoViewComponent,
+    RadioViewComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
