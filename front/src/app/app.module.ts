c47452df2e02f173f42a362a6f3b0bf35449481a
Clément Corbin <corbin.clement@gmail.com>
2020-06-14 15:56:38 +0200

    custom pipe for displaying names

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { MusicViewComponent } from './music-view/music-view.component';
-import { VideoViewComponent } from './video-view/video-view.component';
-import { RadioViewComponent } from './radio-view/radio-view.component';
+import { JukeberryService } from './services/jukeberry.service';
+import { NormalizeNamePipe } from './normalize-name.pipe';
+
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
-import { JukeberryService } from './services/jukeberry.service';
+import { MusicViewComponent } from './music-view/music-view.component';
+import { MusicItemComponent } from './music-item/music-item.component';
+import { VideoViewComponent } from './video-view/video-view.component';
+import { RadioViewComponent } from './radio-view/radio-view.component';
 import { RadioComponent } from './radio/radio.component';
 import { YoutubeViewComponent } from './youtube-view/youtube-view.component';
 import { YoutubeItemComponent } from './youtube-item/youtube-item.component';
@@ -28,6 +31,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    NormalizeNamePipe,
     MusicViewComponent,
     VideoViewComponent,
     RadioViewComponent,
@@ -36,7 +40,8 @@ const appRoutes: Routes = [
     NavComponent,
     RadioComponent,
     YoutubeViewComponent,
-    YoutubeItemComponent
+    YoutubeItemComponent,
+    MusicItemComponent
   ],
   imports: [
     BrowserModule,
