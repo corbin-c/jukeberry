7009abf62f5d102a1dac9f4713d287807a0e490f
Clément Corbin <corbin.clement@gmail.com>
2021-08-15 21:00:34 +0200

    add getRequestBody method

--- server.js
+++ server.js
@@ -3,6 +3,22 @@ const minimalServer = require("@corbin-c/minimal-server");
 //Exposed server
 const server = new minimalServer();
 
+server.getRequestBody = (req) => {
+  return new Promise((resolve,reject) => {
+    let data = "";
+    req.on("data", chunk => {
+      data += chunk;
+    });
+    req.on("end", () => {
+      try {
+        resolve(JSON.parse(data));
+      } catch {
+        resolve(data);
+      }
+    });
+  });
+}
+
 server.failure = (response,code,error) => {
   console.error(code,error);
   response.writeHead(code);
