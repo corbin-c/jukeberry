1a730400293d88047681d2b3aed7890a0b0bfa77
Clément Corbin <corbin.clement@gmail.com>
2021-08-22 15:12:29 +0200

    implemented drag & drop to reorganize playlist

--- front/src/components/PlayListItem.vue
+++ front/src/components/PlayListItem.vue
@@ -1,13 +1,19 @@
 <template>
-  <li>
-    <button @click="play()" v-if="!isPlaylist">
-      <MaterialIcon icon="reorder" />
-    </button>
+  <li
+    :draggable="!isPlaylist"
+    v-on:dragstart="move"
+    v-on:dragenter="dragEnter"
+    v-on:dragleave="dragLeave"
+    v-on:dragend="hasMoved"
+    v-on:dragover="dragover"
+    v-on:drop="drop"
+    :class="classList.join(' ')">
+    <MaterialIcon icon="reorder" v-if="!isPlaylist" />
     <span @click="addToPlaylist()">
       {{ fileName }}
     </span>
     <div class="actions" v-if="isPlaylist && !modal">
-      <button @click="play()">
+      <button @click="playList()">
         <MaterialIcon icon="playlist_play" />
       </button>
       <button @click="shuffle()">
@@ -19,7 +25,7 @@
       <router-link :to="'/playlist/'+id"><MaterialIcon icon="view_list" /></router-link>
     </div>
     <div class="actions" v-else-if="!isPlaylist && !modal">
-      <button @click="play()">
+      <button @click="playSong()">
         <MaterialIcon icon="playlist_play" />
       </button>
       <button @click="remove()">
@@ -35,6 +41,12 @@ import requests from '@/requests.js'
 
 export default {
   name: 'PlayListItem',
+  data() {
+    return {
+      classList: [],
+      isMoving: false
+    }
+  },
   components: {
     MaterialIcon
   },
@@ -50,6 +62,44 @@ export default {
     }
   },
   methods: {
+    dragover(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      e.dataTransfer.dropEffect = 'move';
+      if (!this.classList.includes("over") && !this.isMoving) {
+        this.classList.push('over');
+      }
+    },
+    drop(e) {
+      if (e.preventDefault) {
+        e.preventDefault();
+      }
+      if (e.stopPropagation) {
+        e.stopPropagation();
+      }
+      this.classList = [];
+      this.$parent.organize(this.name,e.dataTransfer.getData('text/plain'));
+    },
+    dragEnter() {
+      if (!this.isMoving) {
+        this.classList.push('over');
+      }
+    },
+    dragLeave() {
+      this.classList = this.classList.filter(e => e != "over");
+    },
+    move(e) {
+      console.log("moving");
+      e.dataTransfer.effectAllowed = 'move';
+      e.dataTransfer.setData('text/plain', this.name);
+      this.classList.push("moving");
+      this.isMoving = true;
+    },
+    hasMoved() {
+      this.classList = this.classList.filter(e => e != "moving");
+      this.isMoving = false;
+    },
     async addToPlaylist() {
       if (this.modal) {
         let response = await requests.addToPlaylist(this.id,this.path);
@@ -73,14 +123,18 @@ export default {
         this.$root.showNotification("La piste '"+this.$root.cleanFileName(this.name)+"' a été supprimée de la playlist.");
       }
     },
-    play() {
-      requests.play(this.context,this.path,this.type);
+    playList() {
+      requests.play("playlist",parseInt(this.id));      
+    },
+    playSong() {
+      requests.play("playlist",parseInt(this.playlistId),parseInt(this.index));
     },
     shuffle() {
-      requests.play(this.context,this.path,this.type,true);
+      requests.play("playlist",parseInt(this.id),0,true);
     }
   },
   props: {
+    index: Number,
     id: Number,
     playlistId: Number,
     name: String,
@@ -89,3 +143,11 @@ export default {
   }
 }
 </script>
+<style scoped>
+li.moving {
+  opacity: .5;
+}
+li.over {
+  padding-top: 100px;
+}
+</style>
