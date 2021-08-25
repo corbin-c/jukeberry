6a473b70f39aa521fcc7f9c2e2ac8c53c3b31f6e
Clément Corbin <corbin.clement@gmail.com>
2021-08-25 22:26:02 +0200

    port detection before start

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
-const jb = require("./jukeberry.js");
-require("./logger.js")(jb.config.log);
+const http = require("http");
+const checkPort = (port) => {
+  return new Promise((resolve, reject) => {
+    const server = http.createServer();
+    server.on("error", (e) => {
+      reject(false);
+      server.close();
+    });
+    server.on("listening", () => {
+      resolve(true);
+      server.close();
+    });
+    server.listen(port);
+  });
+}
+(async () => {
+  try {
+    await checkPort(5000);
+    const jb = require("./jukeberry.js");
+    require("./logger.js")(jb.config.log);
+  } catch(e) {
+    console.error("port already in use. aborting");
+    process.exit();
+  }
+})();
