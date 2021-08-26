faed79401b9bdbcca7a6143ff841e92c53588460
Clément Corbin <corbin.clement@gmail.com>
2021-08-26 08:12:58 +0200

    gpio test

---
+++ gpio/test.js
@@ -0,0 +1,17 @@
+const gpio = require("./gpio.js");
+
+const wait = (t) => {
+  return new Promise((resolve,reject) => {
+    setTimeout(() => {
+      resolve();
+    },t);
+  });
+}
+const alternateBlink = async () => {
+  gpio["led-green"].blink(250);
+  await wait(125);
+  gpio["led-yellow"].blink(250);  
+  await wait(5000);
+  gpio["led-green"].endBlink;
+  gpio["led-yellow"].endBlink;
+}
