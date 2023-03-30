4df40195482dfaa551480460540b0b56dc7d78a2
Clément Corbin <corbin.clement@gmail.com>
2023-03-30 14:52:02 +0200

    gpio

--- gpio/gpio.js
+++ gpio/gpio.js
@@ -10,11 +10,11 @@ const GPIO = class {
     this.buttons = {};
     this.combinations = [];
     this.lastButton = {};
-    this.config.leds.forEach(e => {
+    this.config.leds.forEach((e) => {
       if (this.Gpio !== false) {
         this.leds[e.name] = new this.Gpio(e.port, "out");
-        this["led-"+e.name] = {
-          on: async (alone=false) => {
+        this["led-" + e.name] = {
+          on: async (alone = false) => {
             if (alone) {
               await this.allLedsOff();
             }
@@ -24,8 +24,10 @@ const GPIO = class {
             this.leds[e.name].writeSync(0);
           },
           blink: (t) => {
-            if ((typeof this.blinking[e.name] !== "undefined")
-              && (this.blinking[e.name] !== false)) {
+            if (
+              typeof this.blinking[e.name] !== "undefined" &&
+              this.blinking[e.name] !== false
+            ) {
               console.log(e.name, "already blinking");
               return;
             }
@@ -34,64 +36,69 @@ const GPIO = class {
                 this.leds[e.name].writeSync(1);
               } else {
                 this.leds[e.name].writeSync(0);
-              }              
+              }
             }, t);
           },
           endBlink: () => {
             clearInterval(this.blinking[e.name]);
             this.leds[e.name].writeSync(0);
             this.blinking[e.name] = false;
-          }
-        }
+          },
+        };
       } else {
-        this["led-"+e.name] = {
+        this["led-" + e.name] = {
           on: () => {},
           off: () => {},
           blink: () => {},
           endBlink: () => {},
-        }
+        };
       }
     });
-    this.config.buttons.forEach(e => {
+    this.config.buttons.forEach((e) => {
       if (this.Gpio !== false) {
         this.buttons[e.name] = new this.Gpio(e.port, "in", "both");
-        this["btn-"+e.name] = {
+        this["btn-" + e.name] = {
           pushCallback: () => {},
           releaseCallback: () => {},
           onPush: (callback) => {
-            this["btn-"+e.name].pushCallback = callback;
+            this["btn-" + e.name].pushCallback = callback;
           },
           onRelease: (callback) => {
-            this["btn-"+e.name].releaseCallback = callback;
-          }
+            this["btn-" + e.name].releaseCallback = callback;
+          },
         };
-        this.buttons[e.name].watch((error,value) => {
+        this.buttons[e.name].watch((error, value) => {
           if (error) {
             console.error(error);
             return;
           }
           if (value === 1) {
-            if ((typeof this.buttons[e.name].lastPush === "undefined")
-            || ((new Date()).getTime() - this.buttons[e.name].lastPush > 300)) {
-              this["btn-"+e.name].pushCallback();
-              const combinations = this.combinations
-                .filter(c => c.buttons.includes(e.name));
+            if (
+              typeof this.buttons[e.name].lastPush === "undefined" ||
+              new Date().getTime() - this.buttons[e.name].lastPush > 300
+            ) {
+              this["btn-" + e.name].pushCallback();
+              const combinations = this.combinations.filter((c) =>
+                c.buttons.includes(e.name)
+              );
               if (combinations.length > 0) {
-                combinations.map(async c => {
+                combinations.map(async (c) => {
                   const index = c.buttons.indexOf(e.name);
                   if (index == 0) {
                     c.lastButton = {
                       name: e.name,
-                      time: (new Date()).getTime()
-                    }
+                      time: new Date().getTime(),
+                    };
                   } else {
-                    if (c.lastButton.name == c.buttons[index-1]
-                    && (new Date()).getTime() - c.lastButton.time < 1000) {
+                    if (
+                      c.lastButton.name == c.buttons[index - 1] &&
+                      new Date().getTime() - c.lastButton.time < 1000
+                    ) {
                       c.lastButton = {
                         name: e.name,
-                        time: (new Date()).getTime()
-                      }
-                      if (index == c.buttons.length-1) {
+                        time: new Date().getTime(),
+                      };
+                      if (index == c.buttons.length - 1) {
                         await this.wait(1000);
                         c.callback();
                       }
@@ -100,34 +107,37 @@ const GPIO = class {
                 });
               }
             }
-            this.buttons[e.name].lastPush = (new Date()).getTime();
+            this.buttons[e.name].lastPush = new Date().getTime();
           } else {
-            if ((typeof this.buttons[e.name].lastRelease === "undefined")
-            || ((new Date()).getTime() - this.buttons[e.name].lastRelease > 300)) {
-              this["btn-"+e.name].releaseCallback();
+            if (
+              typeof this.buttons[e.name].lastRelease === "undefined" ||
+              new Date().getTime() - this.buttons[e.name].lastRelease > 300
+            ) {
+              this["btn-" + e.name].releaseCallback();
             }
-            this.buttons[e.name].lastRelease = (new Date()).getTime();          }
+            this.buttons[e.name].lastRelease = new Date().getTime();
+          }
         });
       } else {
-        this["btn-"+e.name] = {
+        this["btn-" + e.name] = {
           pushCallback: () => {},
           releaseCallback: () => {},
           onPush: () => {},
-          onRelease: () => {}
-        }
+          onRelease: () => {},
+        };
       }
     });
     if (this.Gpio !== false) {
       this.stopAllBlinks();
       process.on("SIGINT", () => {
-        Object.values(this.buttons).forEach(btn => {
+        Object.values(this.buttons).forEach((btn) => {
           try {
             btn.unexport();
           } catch (e) {
             console.error(e);
           }
         });
-        Object.values(this.leds).forEach(led => {
+        Object.values(this.leds).forEach((led) => {
           try {
             led.writeSync(0);
             led.unexport();
@@ -140,45 +150,49 @@ const GPIO = class {
     }
   }
   wait(t) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         resolve();
       }, t);
     });
   }
   async allLedsOff() {
-    Object.values(this.leds).map(led => {
-      led.writeSync(0);                
+    Object.values(this.leds).map((led) => {
+      led.writeSync(0);
     });
     await this.wait(100);
     this.stopAllBlinks();
   }
   stopAllBlinks() {
-    this.config.leds.map(e => {
-      this["led-"+e.name].off();
-      this["led-"+e.name].endBlink();
+    this.config.leds.map((e) => {
+      this["led-" + e.name].off();
+      this["led-" + e.name].endBlink();
     });
+  }
+  async niceBlink() {
+    await this.allLedsOff();
+    const t = 250;
+    for (let led of this.config.leds) {
+      this["led-" + led.name].blink(t);
+      await this.wait(t / this.config.leds.length);
+    }
   }
   stop() {
     return new Promise(async (resolve) => {
       await this.allLedsOff();
-      const t = 250;
-      for (let led of this.config.leds) {
-        this["led-"+led.name].blink(t);
-        await this.wait(t/this.config.leds.length);
-      }
+      this.gpio["led-red"].blink(250);
       await this.wait(3000);
       await this.allLedsOff();
-      Object.values(this.buttons).forEach(e => {
+      Object.values(this.buttons).forEach((e) => {
         e.unexport();
       });
-      Object.values(this.leds).forEach(e => {
+      Object.values(this.leds).forEach((e) => {
         e.writeSync(0);
         e.unexport();
       });
       resolve();
     });
   }
-}
+};
 
 module.exports = new GPIO(config, Gpio);
--- jukeberry.js
+++ jukeberry.js
@@ -1,7 +1,7 @@
 const { readFileSync, writeFileSync } = require("fs");
 const { execSync } = require("child_process");
 const websocket = require("websocket-driver");
-const Utils = require("./utils.js")
+const Utils = require("./utils.js");
 const FilesHandler = require("./files.js");
 const MediaPlayer = require("./media.js");
 const PlayListManager = require("./playlist.js");
@@ -18,7 +18,7 @@ class Jukeberry {
     this.gpio["led-red"].on();
     this._status = {
       ready: false,
-      playing: false
+      playing: false,
     };
     this.config = config;
     this.utils = new Utils(this);
@@ -30,13 +30,14 @@ class Jukeberry {
     this.metadata = new Metadata(this);
     this.server = server;
     const routes = require("./router/router.js")(this);
-    routes.map(e => {
+    routes.map((e) => {
       this.server.route = {
-        path: "/api"+e.path,
-        handler: e.hdl
-      }
+        path: "/api" + e.path,
+        handler: e.hdl,
+      };
     });
-    this.server.server.on("upgrade", (request,socket,body) => { //socket to pass current logs
+    this.server.server.on("upgrade", (request, socket, body) => {
+      //socket to pass current logs
       if (!websocket.isWebSocket(request)) return;
       let driver = websocket.http(request);
       driver.io.write(body);
@@ -45,19 +46,17 @@ class Jukeberry {
       this.setSocket(driver);
     });
     this.init();
-    this.gpio.combinations.push(
-      {
-        buttons: ["switch-b-green","switch-b-yellow","switch-b-red"],
-        callback: async () => {
-          await this.media.stop();
-          await this.gpio.stop();
-          console.warn("shutdown triggered");
-          await this.utils.wait(1000);
-          execSync("sudo shutdown -h now");
-        },
-        lastButton: {}
-      }
-    );
+    this.gpio.combinations.push({
+      buttons: ["switch-b-green", "switch-b-yellow", "switch-b-red"],
+      callback: async () => {
+        await this.media.stop();
+        await this.gpio.stop();
+        console.warn("shutdown triggered");
+        await this.utils.wait(1000);
+        execSync("sudo shutdown -h now");
+      },
+      lastButton: {},
+    });
   }
   setSocket(ws) {
     if (typeof this.config.sockets !== "undefined") {
@@ -68,26 +67,28 @@ class Jukeberry {
   }
   async parseLog() {
     const updateStatus = (metadata) => {
-      if ((Object.keys(metadata).length !== 0)
-      && (this.status.playing)) {
+      if (Object.keys(metadata).length !== 0 && this.status.playing) {
         this.status = {
           playing: {
-            metadata
-          }
+            metadata,
+          },
         };
       }
-    }
-    let log = readFileSync("raw.log","utf8");
+    };
+    let log = readFileSync("raw.log", "utf8");
     let path = "";
     let metadata = {};
-    log.split("\n").filter(e => e !== "").map(e => {
-      if (e.indexOf("ANS_") == 0) {
-        path = e.split("=")[1];
-      } else if (e.indexOf("Playing ") == 0) {
-        path = e.split("Playing ")[1].slice(0,-1);
-      }
-    });
-    if ((this.status.playing.mode === "music") && (path)) {
+    log
+      .split("\n")
+      .filter((e) => e !== "")
+      .map((e) => {
+        if (e.indexOf("ANS_") == 0) {
+          path = e.split("=")[1];
+        } else if (e.indexOf("Playing ") == 0) {
+          path = e.split("Playing ")[1].slice(0, -1);
+        }
+      });
+    if (this.status.playing.mode === "music" && path) {
       // 1\ use https://github.com/Borewit/music-metadata to get metadata
       // 1.1\if no metadata: try to deduce from file structure:
       //      parent folder is likely album, its parent being artist
@@ -108,19 +109,24 @@ class Jukeberry {
   sendLog() {
     const prepareStatusToSend = (input, excludedKeys) => {
       let output = {};
-      Object.keys(input).map(k => {
+      Object.keys(input).map((k) => {
         if (!excludedKeys.includes(k)) {
           if (typeof input[k] === "object") {
             if (typeof input[k].length === "undefined") {
-              output[k] = prepareStatusToSend(input[k],excludedKeys);
+              output[k] = prepareStatusToSend(input[k], excludedKeys);
             } else {
-              output[k] = [...input[k].map(e => {
-                if (typeof e === "object" && typeof e.length === "undefined") {
-                  return prepareStatusToSend(e,excludedKeys);
-                } else {
-                  return e;
-                }
-              })];
+              output[k] = [
+                ...input[k].map((e) => {
+                  if (
+                    typeof e === "object" &&
+                    typeof e.length === "undefined"
+                  ) {
+                    return prepareStatusToSend(e, excludedKeys);
+                  } else {
+                    return e;
+                  }
+                }),
+              ];
             }
           } else {
             output[k] = input[k];
@@ -128,10 +134,16 @@ class Jukeberry {
         }
       });
       return output;
-    }
+    };
     if (typeof this.config.sockets !== "undefined") {
-      let status = JSON.stringify(prepareStatusToSend(this.status, ["artistDetails", "albumDetails", "picture"]));
-      this.config.sockets.map(socket => {
+      let status = JSON.stringify(
+        prepareStatusToSend(this.status, [
+          "artistDetails",
+          "albumDetails",
+          "picture",
+        ])
+      );
+      this.config.sockets.map((socket) => {
         try {
           socket.text(status);
         } catch {
@@ -145,15 +157,19 @@ class Jukeberry {
     this.globalList = this.utils.makeGlobalLists(files);
   }
   async init() {
-    setInterval(() => { this.sendLog() },5000);
+    setInterval(() => {
+      this.sendLog();
+    }, 5000);
     try {
       execSync("mkfifo ./mplayer_master");
-    } catch { /* NoOp, named pipe should already exist */ }
+    } catch {
+      /* NoOp, named pipe should already exist */
+    }
     await this.server.enableStaticDir();
     try {
       this.globalList = this.utils.makeGlobalLists();
       console.info("success !");
-    } catch(e) {
+    } catch (e) {
       console.warn(e.message);
       console.info("Building lists from scratch...");
       this.generateTrees();
@@ -169,7 +185,7 @@ class Jukeberry {
     }
     if (typeof this.config.startupSound === "string") {
       try {
-        writeFileSync("./playlist",this.config.startupSound);
+        writeFileSync("./playlist", this.config.startupSound);
         this.media.play("./playlist");
       } catch {
         console.error("Couldn't play startup sound");
@@ -178,14 +194,15 @@ class Jukeberry {
       console.log("no startup sound configured");
     }
     this.status = {
-      ready: true
-    }
+      ready: true,
+    };
   }
   set status(options) {
     if (typeof options.ready !== "undefined") {
       this._status.ready = options.ready;
+      this.gpio.stopAllBlinks();
       if (options.ready === true) {
-        this.gpio["led-yellow"].on(true);
+        this.gpio["led-green"].on(true);
       } else {
         this.gpio["led-red"].on(true);
       }
@@ -194,25 +211,23 @@ class Jukeberry {
       if (options.playing === false) {
         this._status.playing = false;
         this.gpio.stopAllBlinks();
-        this.gpio["led-yellow"].on(true);
+        this.gpio["led-green"].on(true);
       } else {
         if (typeof this._status.playing === "boolean") {
           this._status.playing = {};
         }
-        Object.keys(options.playing).forEach(k => {
+        Object.keys(options.playing).forEach((k) => {
           this._status.playing[k] = options.playing[k];
         });
         if (typeof options.playing.paused !== "undefined") {
           if (options.playing.paused === true) {
             this.gpio.stopAllBlinks();
             (async () => {
-              this.gpio["led-green"].blink(250);
-              await this.gpio.wait(250);
-              this.gpio["led-yellow"].blink(250); 
+              this.gpio["led-yellow"].blink(250);
             })();
           } else {
             this.gpio.stopAllBlinks();
-            this.gpio["led-green"].on(true);
+            this.gpio.niceBlink();
           }
         }
       }
